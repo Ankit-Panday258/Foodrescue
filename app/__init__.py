632b8ebceecc6a3aeb3ca18d65c36e4d75ce204c
Vaibhav <krvaibhav9999@gmail.com>
2025-07-08 14:20:06 +0530

    Models created

--- app/__init__.py
+++ app/__init__.py
@@ -10,19 +10,19 @@ def create_app():
     # Configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  
     app.config['SECRET_KEY'] = 'some key'  
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable warning
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
     # Initialize extensions
     db.init_app(app)
     
+    # Import models (important for migrations)
+    from .models import User, Listing, Claim
     
     from .routes.home import home
     from .routes.food import food
     
     app.register_blueprint(home, url_prefix='/')
     app.register_blueprint(food, url_prefix='/foods')
-
-   
-   
     
     migrate = Migrate(app, db)
     return app
